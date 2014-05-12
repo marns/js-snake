713f03c831b28bfb8651f1eeac7e4ffde4cffe84
marn <martin@asimia.net>
2014-05-12 01:30:09 -0400

    preparing for release

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 cc.game.onStart = function(){
-    cc.view.setDesignResolutionSize(1024, 1024, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(640, 640, cc.ResolutionPolicy.SHOW_ALL);
 	cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.loader.load(g_resources, {}, function() {
         cc.director.runScene(new MainMenuScene());
     });
-
-    /*
-    cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new MainMenuScene());
-    }, this);*/
 };
 cc.game.run();
